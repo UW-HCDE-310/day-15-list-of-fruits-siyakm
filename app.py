ef0ec5c66d67d8820cc021e23d68603b467fff99
Siya Mistry <siyakmistry@gmail.com>
2024-11-19 10:42:03 -0800

    ignoring keys.py

--- app.py
+++ app.py
@@ -8,4 +8,13 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
+    #ORIGINAL
+    #return render_template("index.html", fruits=fruits)
+
+    #FRUITS THAT START WITH "O"
+    # fruits = [fruit for fruit in fruits if fruit["name"].startswith("o")]
+    # return render_template("index.html", fruits=fruits)
+
+    #FRUITS THAT YOU HAVE MORE THAN 3 OF
+    fruits = [fruit for fruit in fruits if fruit["quantity"] > 3]
     return render_template("index.html", fruits=fruits)
